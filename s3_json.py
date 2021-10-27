ebdc4eceb740e08a0549e88dd24adf1d2b6ed344
SwathiPriyaRV23 <89335197+SwathiPriyaRV23@users.noreply.github.com>
2021-10-27 17:19:11 +0530

    Add files via upload

---
+++ s3_json.py
@@ -0,0 +1,39 @@
+import json
+filepath = "C:\\Users\\Administrator\\PycharmProjects\\S3\\test5.yaml"
+data = { "Resources": {
+        "S3Bucket": {
+            "Type": "AWS::S3::Bucket",
+            "Properties": {
+                "AccessControl": "PublicRead",
+                "BucketName": "public-bucket",
+                "MetricsConfigurations": [
+                    {
+                        "Id": "EntireBucket"
+                    }
+                ],
+                "WebsiteConfiguration": {
+                    "IndexDocument": "index.html",
+                    "ErrorDocument": "error.html",
+                    "RoutingRules": [
+                        {
+                            "RoutingRuleCondition": {
+                                "HttpErrorCodeReturnedEquals": "404",
+                                "KeyPrefixEquals": "out1/"
+                            },
+                            "RedirectRule": {
+                                "HostName": "ec2-11-22-333-44.compute-1.amazonaws.com",
+                                "ReplaceKeyPrefixWith": "report-404/"
+                            }
+                        }
+                    ]
+                }
+            },
+            "DeletionPolicy": "Retain"
+        }
+    }, }
+
+
+def json_dump(filepath, data):
+  with open(filepath,"w") as descriptor:
+    json.dump(data, descriptor)
+json_dump(filepath, data)
